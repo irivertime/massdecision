447ef6ca4faac137f8161434dc24faac08785bae
irivertime <irivertime@mail.ru>
2014-01-18 22:03:21 +0400

    init uploud

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Massdecision::Application.config.secret_token = 'db6ce7e2258e07daa305ccc4330648561c7979adaad7c8deb152a292e13ca22b3ef687bca76dfe11e68701781427744f480ac442cdb24b5e257102c4b3dbe941'
